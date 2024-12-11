596c74fd8e6b2bd5f59904ac246f68f96193bf5d
Bottesz <botyaring@gmail.com>
2024-12-11 12:24:20 +0100

    agazati gyakorlo autos

---
+++ main.py
@@ -0,0 +1,25 @@
+import bevezetes
+import sorozat
+import autom
+
+bevezetes.elso_feladat()
+sorozat.masodik_feladat()
+eredmeny=sorozat.egyjegyuek_szama()
+print()
+print(f"\t Egyjegyűek szama: {eredmeny} ")
+sorozat.fajlba_kiir()
+
+
+
+
+autok = autom.beolvas("auto.txt")
+print("III/Flotta:")
+print(f"Autók száma: {autom.flotta(autok)}.")
+
+legfiatalabb_auto = autom.legfiatalabb(autok)
+print("III/Legfiatalabb:")
+print(f"A legfiatalabb autó: {legfiatalabb_auto.nev} ({legfiatalabb_auto.gyartasi_ev}).")
+
+atlag_kor = autom.atlagos_kor(autok)
+print("III/Átlag kor:")
+print(f"Az autók átlagos kora: {atlag_kor:.2f} év.")
